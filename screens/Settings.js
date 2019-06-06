7b34b4f1d3171084484d101381a367b02ec89815
SmitOanea <smitoanea@gmail.com>
2019-06-06 16:14:54 +0300

    added gray

--- screens/Settings.js
+++ screens/Settings.js
@@ -5,6 +5,7 @@ import {
 } from "react-native";
 import ToggleSwitch from "toggle-switch-react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import Toast from 'react-native-easy-toast';
 
 import LoginButton from '../components/LoginButton';
 import DataManager from '../utils/DataManager';
@@ -39,7 +40,7 @@ export default class Settings extends Component {
   saveSettings = () => {
     if (this.state.key && this.state.name != '' && this.state.email != '') {
       this.dataManager.updateUserDetails(this.dataManager.getUserID(), this.state);
-      alert("Settings saved!");
+      this.refs.toast("Settings saved!");
     }
   }
 
@@ -48,7 +49,8 @@ export default class Settings extends Component {
     const { goBack } = this.props.navigation;
 
     return (
-      <View style={styles.container}>
+        <View style={styles.container}>
+        <Toast ref="toast" position='center' />
         <StatusBar barStyle='light-content' />
         <View style={styles.header}>
           <TouchableOpacity style={styles.backButton} onPress={() => goBack()}>
@@ -56,84 +58,113 @@ export default class Settings extends Component {
           </TouchableOpacity>
           <Text style={styles.headerText}>Settings</Text>
         </View>
-        <View style={styles.textfieldCard}>
-          <Text style={styles.label}>Email: </Text>
-          <TextInput
-            editable={false}
-            style={styles.textInput}
-            onChangeText={email => this.setState({ email })}
-            value={email}
-            autoCapitalize='none'
-          />
-        </View>
-        <View style={styles.textfieldCard}>
-          <Text style={styles.label}>Name: </Text>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={name => this.setState({ name })}
-            value={name}
-            autoCapitalize='none'
-          />
-        </View>
-        <View style={styles.textfieldCard}>
-          <Text style={styles.label}>Course: </Text>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={course => this.setState({ course })}
-            value={course}
-            autoCapitalize='none'
-          />
-        </View>
-        <View style={styles.textAreaContainer}>
-          <Text style={styles.label}>Details: </Text>
-          <TextInput
-            style={styles.textArea}
-            underlineColorAndroid="transparent"
-            placeholder="Add details about you"
-            placeholderTextColor="grey"
-            multiline={true}
-            value={details}
-            onChangeText={details => this.setState({ details })}
-            returnKeyType='done'
-            blurOnSubmit={true}
-          />
-        </View>
-        <View style={styles.toggleButton}>
-          <ToggleSwitch
-            isOn={isTeacher}
-            onColor="#00BFFF"
-            offColor="#ededed"
-            size="medium"
-            label="Teacher: "
-            labelStyle={{ color: "#00BFFF", fontWeight: "bold" }}
-            onToggle={isOn => {
-              this.setState({ isTeacher: isOn });
-            }}
-          />
-        </View>
-        <View style={styles.toggleButton}>
-          <ToggleSwitch
-            isOn={shareLocation}
-            onColor="#00BFFF"
-            offColor="#ededed"
-            size="medium"
-            label="Share your location: "
-            labelStyle={{ color: "#00BFFF", fontWeight: "bold" }}
-            onToggle={isOn => {
-              var location = null;
-              if (isOn) {
-                location = this.dataManager.getLocation();
-              }
 
-              this.setState({ shareLocation: isOn, location });
-            }}
-          />
+        {/*
+          <View style={styles.teacher}>
+            <ToggleSwitch
+              isOn={isTeacher}
+              onColor="#00BFFF"
+              offColor="#ededed"
+              size="medium"
+              label="Teacher: "
+              labelStyle={{ color: "#00BFFF", fontWeight: "bold" }}
+              onToggle={isOn => {
+                this.setState({ isTeacher: isOn });
+              }}
+            />
+          </View>
+        */}
+        <View backgroundColor="#D8D8D8">
+          <View style={styles.textfieldCard}>
+            <Icon name="ios-mail" color="black" size={21} />
+            <Text style={styles.label}>Emaill: </Text>
+            <TextInput
+              editable={false}
+              style={styles.textInput}
+              onChangeText={email => this.setState({ email })}
+              value={email}
+              autoCapitalize='none'
+            />
+          </View>
+          <View style={styles.textfieldCard}>
+            <Icon name="md-contact" color="black" stylee ={styles.icon} size={21} />
+            <Text style={styles.label}>Name: </Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={name => this.setState({ name })}
+              value={name}
+              autoCapitalize='none'
+            />
+          </View>
+          <View style={styles.textfieldCard}>
+            <View style={styles.icon}>
+              <Icon name="ios-school" color="black" size={21} />
+            </View>
+            <Text style={styles.label}>Course: </Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={course => this.setState({ course })}
+              value={course}
+              autoCapitalize='none'
+            />
+          </View>
+          <View style={styles.textAreaContainer}>
+            <Text style={styles.label}>Details: </Text>
+            <TextInput
+              style={styles.textArea}
+              underlineColorAndroid="transparent"
+              placeholder="Add details about you"
+              placeholderTextColor="grey"
+              multiline={true}
+              value={details}
+              onChangeText={details => this.setState({ details })}
+              returnKeyType='done'
+              blurOnSubmit={true}
+            />
+          </View>
+          <View style={styles.toggleButton}>
+            <ToggleSwitch
+              isOn={isTeacher}
+              onColor="#00BFFF"
+              offColor="#ededed"
+              size="medium"
+              label="Teacher: "
+              labelStyle={{ color: "#00BFFF", fontWeight: "bold" }}
+              onToggle={isOn => {
+                this.setState({ isTeacher: isOn });
+              }}
+            />
+          </View>
+          <View style={styles.toggleButton}>
+            <ToggleSwitch
+              isOn={shareLocation}
+              onColor="#00BFFF"
+              offColor="#ededed"
+              size="medium"
+              label="Share your location: "
+              labelStyle={{ color: "#00BFFF", fontWeight: "bold" }}
+              onToggle={isOn => {
+                var location = null;
+                if (isOn) {
+                  location = this.dataManager.getLocation();
+                }
+
+                this.setState({ shareLocation: isOn, location });
+              }}
+            />
+          </View>
+          <View backgroundColor='white'>
+            <LoginButton
+              onPress={this.saveSettings}
+              text={"Save Settings"}
+              buttonStyle={{ width: 140 }} />
+          </View>
         </View>
-        <LoginButton
-          onPress={this.saveSettings}
-          text={"Save Settings"}
-          buttonStyle={{ width: 140 }} />
+        <View backgroundColor="#D8D8D8">
+
+        </View>
       </View>
+
     );
   }
 }
@@ -141,39 +172,64 @@ export default class Settings extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white'
   },
   textfieldCard: {
+    backgroundColor: "white",
+    borderColor: "#D8D8D8",
+    borderBottomWidth: 1,
+    margin: 1,
+    padding: 2,
     flexDirection: 'row',
-    justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 1,
-    borderColor: 'red',
-    margin: 3
+    justifyContent: 'center'
+
+
+
+
   },
   textInput: {
-    width: 100,
-    borderColor: "black",
+    width: 200,
+    borderColor: "#D8D8D8",
     borderBottomWidth: 1,
-    margin: 5,
+    margin: 1,
     paddingLeft: 5
   },
   label: {
     width: 60
   },
+  icon: {
+    margin: 0,
+    paddingLeft: 0
+  },
   textAreaContainer: {
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 5,
+    backgroundColor:"white",
+    borderColor: "#D8D8D8",
+    borderBottomWidth: 1,
+    margin: 1,
+    padding: 2,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center'
+
   },
   textArea: {
     flex: 1,
     paddingBottom: 6,
+    borderColor: '#D8D8D8',
+    borderWidth: 1,
+    margin: 1,
+    padding: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   toggleButton: {
-    marginTop: 10,
+    margin: 1,
+    marginTop: 1,
+    paddingTop:5,
+    paddingBottom:5,
+    backgroundColor:'white'
   },
   backButton: {
     position: 'absolute',
@@ -188,6 +244,15 @@ const styles = StyleSheet.create({
     zIndex: 10,
     flexDirection: "row",
     backgroundColor: "#00BFFF",
+    alignItems: "center",
+    justifyContent: "center",
+    width: WIDTH
+  },
+  teacher: {
+    height: 38,
+    zIndex: 10,
+    flexDirection: "row",
+    backgroundColor: "#D8D8D8",
     alignItems: "center",
     justifyContent: "center",
     width: WIDTH
